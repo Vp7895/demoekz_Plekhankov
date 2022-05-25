b58f2cd61298b45626591093fd576b3b66970ec1
Vanya984 <plekhankov68@gmail.com>
2022-05-25 17:16:28 +0300

    навигация работает

---
+++ Plekhankov/Plekhankov/Page/PostachikPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Plekhankov.DateBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Plekhankov.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для PostachikPage.xaml
+    /// </summary>
+    public partial class PostachikPage : Window
+    {
+        public PostachikPage()
+        {
+            InitializeComponent();
+
+            DataGridPost.ItemsSource = Plekhankov_DemoEkzEntities.GetContext().Postavchiki.ToList();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window bckWindow = new MainWindow();
+            bckWindow.Show();
+            Close();
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
